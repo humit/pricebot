6ff0fd4e5174eab81900f12e379be99f482d1aba
humite <humite@gmail.com>
2022-03-27 13:06:40 +0300

    latest changes

--- c_egemalt.sh
+++ c_egemalt.sh
@@ -5,14 +5,16 @@ OUTPRD=/tmp/egemalt_prd.html
 URL_PFX=https://egemalt.com
 URL_SFX="/bandit-crow/"
 IFS_OLD=$IFS
+UA="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
 
-curl -s -k -A 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0' "${URL_PFX}${URL_SFX}" > ${OUTPUT}
+curl -s -k -A "${UA}" "${URL_PFX}${URL_SFX}" > ${OUTPUT}
 
 while IFS= read URL; do
- curl -s -k -A 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0' "${URL}" > ${OUTPRD}
+ curl -s -k -A "${UA}" "${URL}" > ${OUTPRD}
  TITLE=$(grep og:title ${OUTPRD} |cut -d\" -f4)
- PRICE=$(grep product:price:amount ${OUTPRD}|cut -d\" -f4|tr '.' ','|xargs printf "%.2f"|tr ',' '.')
  HREF=${URL}
+#PRICE=$(grep product:price:amount ${OUTPRD}|cut -d\" -f4|tr '.' ','|xargs printf "%.2f"|tr ',' '.')
+ PRICE=$(grep product:price:amount ${OUTPRD}|cut -d\" -f4)
  echo "$(gdate +%Y-%m-%d\ %H:%M:%S.%3N);${TITLE};${PRICE};${HREF}"
 done< <(cat ${OUTPUT} | pup '.elementor-image-box-wrapper'|grep href|cut -d\" -f2| sort -u)|\
 sort -u|\
