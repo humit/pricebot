a1aacecde8d74958f245cc5352c2966d048c4330
humite <humite@gmail.com>
2022-03-31 00:49:58 +0300

    generate charts with rrdtool

--- topla.sh
+++ topla.sh
@@ -3,24 +3,43 @@
 PRICES="fiyatlar.csv"
 P_HTML="fiyatlar.html"
 SQLTDB="fiyatlar.sqlite"
+LOGFILE="pricebot.log"
 
 /bin/rm -f ${PRICES}
+
+function mk_log()
+{
+    echo "`date '+%m/%d %H:%M:%S'` $1" | tee -a ${LOGFILE}
+}
 
 # Run all crawlers
 run_crawlers(){
  for script in c_*.sh; do
+ mk_log " + running ${script}"
   . ./${script} >> ${PRICES}
  done
 }
 
 # Import CSV into sqlite
 import_csv(){
- echo "Importing ${PRICES} into ${SQLTDB}"
+ mk_log "Importing ${PRICES} into ${SQLTDB}"
  sqlite3 -batch ${SQLTDB} <<EOF
 .separator ";"
 .import ${PRICES} fiyatlar
 EOF
 }
 
-run_crawlers
-import_csv
+write_charts(){
+mk_log " + generating charts"
+while IFS= read -r name; do
+ # Cleanup extra spaces
+ name=$(echo $name|tr -s \ |sed -e 's/^ //')
+ echo ${name}
+ sqlite3 ${SQLTDB} "SELECT id, price FROM fiyatlar WHERE name like '%${name}%'"
+done< <(sqlite3 ${SQLTDB} 'SELECT name FROM fiyatlar'|sort | uniq)
+#done< <(sqlite3 ${SQLTDB} 'SELECT name FROM fiyatlar'|sort | uniq -c|sort -nr)
+}
+
+#run_crawlers
+#import_csv
+write_charts
